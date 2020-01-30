7ba84104d21e31ca14dd4147ecad5b4fbb3d8bc4
Anh Nguyen <soaptavish1993@gmail.com>
2020-01-30 13:08:41 +0700

    recaptcha
    Add recaptcha

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Login.css';
-import ReCAPTCHA from "react-google-recaptcha";
-function onChange(value) {
-  console.log("Captcha value:", value);
-}
+import { ReCaptcha } from 'react-recaptcha-google'
 class Login extends React.Component {
+  constructor( props, context ) {
+    super( props, context );
+    this.onLoadRecaptcha = this.onLoadRecaptcha.bind(this);
+    this.verifyCallback = this.verifyCallback.bind(this);
+  }
+  componentDidMount() {
+    if (this.captchaDemo) {
+        console.log("started, just a second...")
+        this.captchaDemo.reset();
+    }
+  }
+  onLoadRecaptcha() {
+    if (this.captchaDemo) {
+        this.captchaDemo.reset();
+    }
+  }
+  verifyCallback(recaptchaToken) {
+    // Here you will get the final recaptchaToken!!!  
+    console.log(recaptchaToken, "<= your recaptcha token")
+  }
   render() {
     return (
       <div className="login">
@@ -28,9 +45,13 @@ class Login extends React.Component {
                 <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
             </div>
         </div>
-        <ReCAPTCHA
-          sitekey="6LcLG9QUAAAAANA0qrt1NlyVCZmO5yC6sBvuYVUn"
-          onChange={onChange}
+        <ReCaptcha
+            // ref={(el) => {this.captchaDemo = el;}}
+            size="normal"
+            render="explicit"
+            sitekey="6LfPHNQUAAAAALqSDV5Hc-gf9qiVQt0Ii9TTOPGK"
+            // onloadCallback={this.onLoadRecaptcha}
+            // verifyCallback={this.verifyCallback}
         />
         <button type="submit" className="btn btn-primary btn-block">Submit</button>
         <p className="forgot-password text-right">
