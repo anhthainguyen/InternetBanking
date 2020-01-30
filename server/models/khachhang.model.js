911f4dfcfc92923613fe7c618e55200bd8dd861d
ThaiQuangKhai <vnthai084@gmail.com>
2020-01-30 12:23:04 +0700

    apiKhachHang

---
+++ server/models/khachhang.model.js
@@ -0,0 +1,25 @@
+const db = require('../utils/db');
+
+module.exports = {
+  all: () => {
+    const sql = 'select * from khachhang';
+    return db.load(sql);
+  },
+
+  loadById: id => {
+    const sql = `select * from khachhang where idKhachHang = ${id}`;
+    return db.load(sql);
+  },
+
+  deleteById: id => {
+    const sql = `delete from khachhang where idKhachHang = ${id}`;
+    return db.load(sql);
+  },
+
+  add: entity => db.add(entity, 'khachhang'),
+
+  patch: (id, entity) => {
+    delete entity.CatID;
+    return db.patch(entity, { idKhachHang: id }, 'tkdangnhap')
+  },
+};
