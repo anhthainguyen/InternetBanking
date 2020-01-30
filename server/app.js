7f5b7e100ee2549895dcc9588529ef0618f01d65
Anh Nguyen <soaptavish1993@gmail.com>
2020-01-30 11:39:14 +0700

    add api description
    khachhang api

--- server/app.js
+++ server/app.js
@@ -3,6 +3,9 @@ var app = express();
 app.get("/url", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
+app.get("/khachhang", (req, res, next) => {
+    res.json(["Tony","Lisa","Michael","Ginger","Food"]);
+});
 app.listen(3000, () => {
  console.log("Server running on port 3000");
 });
