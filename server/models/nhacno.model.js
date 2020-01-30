e5e83cff177379f63fe2e4f0c0655ffea5d582f9
ThaiQuangKhai <vnthai084@gmail.com>
2020-01-30 21:21:28 +0700

    apiNhacNo

---
+++ server/models/nhacno.model.js
@@ -0,0 +1,25 @@
+const db = require('../utils/db');
+
+module.exports = {
+  all: () => {
+    const sql = 'select * from nhacno';
+    return db.load(sql);
+  },
+
+  loadById: id => {
+    const sql = `select * from nhacno where idNhacNo = ${id}`;
+    return db.load(sql);
+  },
+
+  deleteById: id => {
+    const sql = `delete from nhacno where idNhacNo = ${id}`;
+    return db.load(sql);
+  },
+
+  add: entity => db.add(entity, 'nhacno'),
+
+  patch: (id, entity) => {
+    delete entity.CatID;
+    return db.patch(entity, { idNhacNo: id }, 'nhacno')
+  },
+};
