8bc1c3fcc0076e0145b95b9afb1c81f863aeb394
Anh Nguyen <soaptavish1993@gmail.com>
2020-02-06 23:15:24 +0700

    Experience
    Add redux

--- client/src/HomePage/HomePage.jsx
+++ client/src/HomePage/HomePage.jsx
@@ -43,3 +43,17 @@ class HomePage extends React.Component {
         );
     }
 }
+
+function mapState(state) {
+    const { users, authentication } = state;
+    const { user } = authentication;
+    return { user, users };
+}
+
+const actionCreators = {
+    getUsers: userActions.getAll,
+    deleteUser: userActions.delete
+}
+
+const connectedHomePage = connect(mapState, actionCreators)(HomePage);
+export { connectedHomePage as HomePage };
