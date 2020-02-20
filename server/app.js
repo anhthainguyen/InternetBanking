ede3d86dbf91358566f2946c6ca88a07c5489470
ThaiQuangKhai <vnthai084@gmail.com>
2020-02-20 22:51:03 +0700

    Khiểm Tra rối tin có đúng không

--- server/app.js
+++ server/app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
+const openpgp = require('openpgp');
+const bcrypt = require('bcryptjs');
+const moment = require('moment');
 require('express-async-errors');
 
 const app = express();
@@ -9,7 +12,7 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.json());
 
-app.get('/', (req, res) => {
+app.get('/',(req, res) => {
   res.json({
     msg: 'hello from nodejs express api'
   });
@@ -18,21 +21,49 @@ app.get('/', (req, res) => {
 app.use('/api/auth', require('./routes/auth.route'));
 app.use('/api/tkdangnhap', require('./routes/tkdangnhap.route'));
 
+
+
 function verifyAccessToken(req, res, next) {
   // console.log(req.headers);
   const token = req.headers['x-access-token'];
+  const code = req.headers['code'];
+  const time = req.headers['time'];
+  const hash = req.headers['hash'];
+  const c = code + time;
+  //next();
+  
+
   if (token) {
     jwt.verify(token, 'shhhhh', function (err, payload) {
       if (err) throw createError(403, err);
+      const now = moment();
+      const after = now.clone().subtract(30, 'seconds');
+      if (bcrypt.compareSync(c, hash) && after.format('YYYY-MM-DD HH:mm:ss')<time && time < now.format('YYYY-MM-DD HH:mm:ss')) {
+        next();
+      }else {
+        throw createError(401, 'HASH_ERROR');
+      }
+      //console.log(payload);
 
-      console.log(payload);
-      next();
+      //next();
     });
   } else {
     throw createError(401, 'NO_TOKEN');
   }
+  
+  // const c = entity.code + entity.time;
+  // if (hash) {
+  //   const hash = bcrypt.hashSync(hash, 8);
+  //   if (!bcrypt.compareSync(c, hash)) {
+  //     throw createError(300, 'Saaaaaaaaaaa');
+  //   }
+  //   else{
+  //     throw createError(200, 'TC');
+  //   }
+  // } else {
+  //   throw createError(401, 'NO_HASH');
+  // }
 }
-
 
 app.use('/api/khachhang', verifyAccessToken, require('./routes/khachhang.route'));
 app.use('/api/nguoinhan', verifyAccessToken, require('./routes/nguoinhan.route'));
